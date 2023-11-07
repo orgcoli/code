680c5fc01537f8a1db6a75373a448de54c752f27
woori <alsgh9947@gmail.com>
2023-11-07 12:04:06 +0900

    23-11-07 로그인 작업완료, 이미지 작업중

--- src/main/java/com/example/movie_cl_23_11_06/Controller/MainController.java
+++ src/main/java/com/example/movie_cl_23_11_06/Controller/MainController.java
@@ -1,13 +1,15 @@
 package com.example.movie_cl_23_11_06.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main()throws Exception{
+    public String main(String errorMessage, Model model)throws Exception{
+        model.addAttribute("errorMessage", errorMessage);
         return "index";
     }
 }
---
+++ src/main/java/com/example/movie_cl_23_11_06/Repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.example.movie_cl_23_11_06.Repository;
+
+import com.example.movie_cl_23_11_06.Entity.MovieEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<MovieEntity, Integer> {
+}
